5e7b4b6dd8531ce1ad844d520c4c9c9a36f8bdd4
nihadmusa <nihad.musa41@gmail.com>
2026-09-11 15:52:14 +0400

    Initial commit

---
+++ src/test/java/az/abb/embassyflow/EmbassyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package az.abb.embassyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbassyFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
